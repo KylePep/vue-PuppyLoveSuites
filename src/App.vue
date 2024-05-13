f69ab56f5f602bfe1e2faee581af7e3a079096d7
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-05-13 10:35:17 -0600

    revert commit

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,114 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/NavBar.vue'
 </script>
 
 <template>
+
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <Navbar />
   </header>
 
-  <RouterView />
+  <main class="container">
+    <router-view />
+  </main>
+
+  <footer class="container-fluid d-flex bg-secondary text-light ">
+    <div class="row flex-grow-1 align-items-center justify-content-center py-5">
+      <div class="col-12 text-center fw-bold fs-4">
+        <router-link :to="{ name: 'Home' }" class="text-light text-center selectable fw-bold fs-4">
+          Puppy Love Suites
+        </router-link>
+      </div>
+
+      <div class="col-6 d-flex justify-content-end px-0">
+        <i class="mdi mdi-facebook fs-2 text-light py-1 px-2 selectable"></i>
+        <i class="mdi mdi-instagram fs-2 text-light py-1 px-2 selectable"></i>
+        <i class="mdi mdi-paw fs-2 text-light py-1 px-2 selectable"></i>
+      </div>
+      <div class="col-6 d-flex justify-content-start px-0">
+        <p class="fs-6 fw-semibold text-light bg-primary rounded-4 px-2 py-2 mb-0 selectable ">(208) 353-5939</p>
+      </div>
+      <router-link :to="{ name: 'Contact' }" class="btn text-light lighten-30 selectable text-uppercase">
+        10330 Lake Shore Dr, Nampa, ID 83686 | puppylovesuites@gmail.com
+      </router-link>
+
+      <div class="col-12">
+        <p class=" text-center">
+          Puppy Love Copyright Stuff
+        </p>
+      </div>
+    </div>
+  </footer>
+
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style lang="scss">
+@import "./assets/scss/main.scss";
+
+:root {
+  --main-height: calc(100vh - 32px - 64px);
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+footer {
+  // min-height: 256px;
+  margin-top: 32px;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+h2 {
+  background-color: var(--bs-primary);
+  color: white;
+  // text-shadow: 1px 1px 2px black;
+  padding: 1rem 1rem 1rem 1rem;
+  border-radius: 0px 0px 16px 16px;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.glassmorph {
+  /* From https://css.glass */
+  background: rgba(0, 0, 0, 0.35);
+  // border-radius: 16px;
+  // box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(1.25px);
+  -webkit-backdrop-filter: blur(2px);
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+/* Extra small devices (phones, 600px and down) */
+@media only screen and (max-width: 600px) {
+  article {
+    padding: 0rem 1rem 0rem 1rem;
+  }
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+/* Small devices (portrait tablets and large phones, 600px and up) */
+@media only screen and (min-width: 600px) {
+  article {
+    padding: 0rem 3rem 0rem 3rem;
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
+  /* No changes needed */
+
+}
+
+/* Medium devices (landscape tablets, 768px and up) */
+@media only screen and (min-width: 768px) {
+  article {
+    padding: 0rem 4rem 0rem 4rem;
   }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  /* No changes needed */
+
+}
+
+/* Large devices (laptops/desktops, 992px and up) */
+@media only screen and (min-width: 992px) {
+  article {
+    padding: 0rem 6rem 0rem 6rem;
   }
+}
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+/* Extra large devices (large laptops and desktops, 1200px and up) */
+@media only screen and (min-width: 1200px) {
+  article {
+    padding: 0rem 12rem 0rem 12rem;
   }
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,89 @@
+<script setup>
+// import router from "@/router/index.js";
+
+// import { onMounted, ref } from 'vue';
+// import { loadState, saveState } from '../utils/Store.js';
+// import Login from './Login.vue';
+
+// const theme = ref(loadState('theme') || 'light')
+
+// onMounted(() => {
+//   document.documentElement.setAttribute('data-bs-theme', theme.value)
+// })
+
+// function toggleTheme() {
+//   theme.value = theme.value == 'light' ? 'dark' : 'light'
+//   document.documentElement.setAttribute('data-bs-theme', theme.value)
+//   saveState('theme', theme.value)
+// }
+
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-sm navbar-dark bg-secondary px-3">
+    <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
+      <div class="title d-flex flex-column align-items-center  fw-bold">
+        Puppy Love Suites
+      </div>
+    </router-link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
+      aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto">
+        <li>
+          <router-link :to="{ name: 'Boarding' }" class="btn text-primary lighten-30 selectable text-uppercase">
+            Boarding
+          </router-link>
+        </li>
+        <li>
+          <router-link :to="{ name: 'Contact' }" class="btn text-primary lighten-30 selectable text-uppercase">
+            Contact Us
+          </router-link>
+        </li>
+        <li>
+          <router-link :to="{ name: 'About' }" class="btn text-primary lighten-30 selectable text-uppercase">
+            About
+          </router-link>
+        </li>
+      </ul>
+
+      <div>
+        <i class="mdi mdi-facebook fs-3 text-light me-2 selectable"></i>
+        <i class="mdi mdi-instagram fs-3 text-light me-2 selectable"></i>
+        <i class="mdi mdi-paw fs-3 text-light selectable"></i>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.title {
+  color: var(--bs-light);
+}
+
+.title:hover {
+  color: var(--bs-primary)
+}
+
+a:hover {
+  text-decoration: none;
+}
+
+.nav-link {
+  text-transform: uppercase;
+}
+
+.navbar-nav .router-link-exact-active {
+  border-bottom: 2px solid var(--bs-primary);
+  border-bottom-left-radius: 0;
+  border-bottom-right-radius: 0;
+}
+
+@media screen and (min-width: 576px) {
+  nav {
+    height: 64px;
+  }
+}
+</style>
