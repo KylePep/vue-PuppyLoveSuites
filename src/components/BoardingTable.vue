29ae7e27db739af8c34596cc160671a06717dadd
Kyle Peppersack <133170817+KylePep@users.noreply.github.com>
2024-05-17 10:14:43 -0600

    Created boarding table component, bolded headers, created boarding item component

---
+++ src/components/BoardingTable.vue
@@ -0,0 +1,81 @@
+<template>
+  <table class="table border border-primary border-4 rounded my-0">
+    <thead class="">
+      <th scope="col">Item</th>
+      <th scope="col">Rate</th>
+      <th scope="col">Description</th>
+    </thead>
+    <tbody>
+
+      <tr class="table-primary">
+        <td scope="row">Single Dog</td>
+        <td>$40 per night</td>
+        <td>
+          <ul>
+            <li>One Suite</li>
+            <li>One Dog</li>
+          </ul>
+        </td>
+      </tr>
+
+      <tr class="table-light">
+        <td scope="row">Additional dog(s)</td>
+        <td>$33 per dog, per night</td>
+        <td>
+          <ul>
+            <li>One Suite</li>
+            <li>more than one dog</li>
+            <li>must get along</li>
+          </ul>
+        </td>
+      </tr>
+
+      <tr class="table-primary">
+        <td scope="row">Sunday and after hours pick up or drop off</td>
+        <td>$50</td>
+        <td>
+          <ul>
+            <li>Must call ahead of time</li>
+          </ul>
+        </td>
+      </tr>
+
+      <tr class="table-light">
+        <td scope="row">Medication administration</td>
+        <td>$3 per day</td>
+        <td>
+          <ul>
+            <li>Medication is not provided</li>
+            <li>Must be able to provide dosage and frequency</li>
+          </ul>
+        </td>
+      </tr>
+
+      <tr class="table-primary">
+        <td scope="row">Food provided by Puppy Love Suites</td>
+        <td>$5 per day</td>
+        <td>
+          <ul>
+            <li>X dog foods to choose from</li>
+            <li>Brands are selected by Puppy Love Suites</li>
+            <li>It is encouraged to provide your dogs regular food</li>
+          </ul>
+        </td>
+      </tr>
+
+    </tbody>
+  </table>
+
+</template>
+
+
+<script>
+export default {
+  setup() {
+    return {}
+  }
+}
+</script>
+
+
+<style lang="scss" scoped></style>
--- src/views/BoardingView.vue
+++ src/views/BoardingView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import BoardingCarousel from "../components/BoardingCarousel.vue";
+import BoardingTable from "../components/BoardingTable.vue";
 
 
 </script>
@@ -47,77 +48,11 @@ import BoardingCarousel from "../components/BoardingCarousel.vue";
 
     <div class="col-12">
       <div class="row g-2">
-        <h3 class="col-12">PRICING</h3>
+        <h3 class="col-12 fw-bold">BOARDING RATES</h3>
 
 
         <div class="col-12 order-2 order-lg-1 col-lg-6 ">
-
-          <table class="table border border-primary border-4 rounded my-0">
-            <thead class="">
-              <th scope="col">Item</th>
-              <th scope="col">Rate</th>
-              <th scope="col">Description</th>
-            </thead>
-            <tbody>
-
-              <tr class="table-primary">
-                <td scope="row">Single Dog</td>
-                <td>$40 per night</td>
-                <td>
-                  <ul>
-                    <li>One Suite</li>
-                    <li>One Dog</li>
-                  </ul>
-                </td>
-              </tr>
-
-              <tr class="table-light">
-                <td scope="row">Additional dog(s)</td>
-                <td>$33 per dog, per night</td>
-                <td>
-                  <ul>
-                    <li>One Suite</li>
-                    <li>more than one dog</li>
-                    <li>must get along</li>
-                  </ul>
-                </td>
-              </tr>
-
-              <tr class="table-primary">
-                <td scope="row">Sunday and after hours pick up or drop off</td>
-                <td>$50</td>
-                <td>
-                  <ul>
-                    <li>Must call ahead of time</li>
-                  </ul>
-                </td>
-              </tr>
-
-              <tr class="table-light">
-                <td scope="row">Medication administration</td>
-                <td>$3 per day</td>
-                <td>
-                  <ul>
-                    <li>Medication is not provided</li>
-                    <li>Must be able to provide dosage and frequency</li>
-                  </ul>
-                </td>
-              </tr>
-
-              <tr class="table-primary">
-                <td scope="row">Food provided by Puppy Love Suites</td>
-                <td>$5 per day</td>
-                <td>
-                  <ul>
-                    <li>X dog foods to choose from</li>
-                    <li>Brands are selected by Puppy Love Suites</li>
-                    <li>It is encouraged to provide your dogs regular food</li>
-                  </ul>
-                </td>
-              </tr>
-
-            </tbody>
-          </table>
+          <BoardingTable />
         </div>
 
         <div class="col-12 d-flex order-1 order-lg-2 col-lg-6 ">
@@ -137,8 +72,8 @@ import BoardingCarousel from "../components/BoardingCarousel.vue";
 
     <div class="col-12">
       <div class="row">
-        <h3 class="col-12">
-          Suites
+        <h3 class="col-12 fw-bold">
+          SUITES
         </h3>
       </div>
       <div class="col-12">
